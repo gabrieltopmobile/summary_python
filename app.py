a0bd2882519c895e4615be510e34a2a3b9dcbcfe
bugz <andrezaycev@gmail.com>
2017-08-09 22:59:25 +0900

    findurl remove symbols

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from gensim.summarization import summarize
 from bottle import route, run, response, Bottle, request
 import json
 from collections import OrderedDict
+import re
+import string
 
 app = Bottle()
 
@@ -27,14 +29,19 @@ def index():
 def api():
     status = "true"
     message = "Nothing"
-    text = request.forms.get('text')
+    #text = request.forms.text.replace("\\n", " ").replace("\\t"," ").replace("  ", " ")
+    text = request.forms.text
+    for i in [r'\\n', r'\\t',r'•']:
+        text = re.sub(i ," ", text)
+    text = re.sub(r'\s+', ' ' , text)
     print (text)
+    print (find_url(text))
     try:
         message = summarize(text)
     except Exception as e:
         status = "false"
         message = str(e)
-
+    response.content_type = 'application/json'
     return json.dumps(OrderedDict(status=status, message=message))
 
 
@@ -45,6 +52,8 @@ def error405(error):
     message = "405 Method not allowed"
     return json.dumps(OrderedDict(status=status, message=message))
 
+def find_url(text):
+    return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port="9000", debug=False)
--- posttest.py
+++ posttest.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-with open('testtext2',"rb") as fin:
+with open('testtext2',"r") as fin:
     text=fin.read()
-r = requests.post("http://10.4.12.35:9000/api", data={'text': text})
-print (json.loads(r.text)['message'])
+print (text)
+r = requests.post("http://10.4.12.35:9000/api", data={'text': text}, headers={"Content-Type": "application/text-plain; charset=UTF-8" })
+
 
