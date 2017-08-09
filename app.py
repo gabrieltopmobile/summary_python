be06df25665a4b80aa82fbc997a95e7812b5f64d
bugz <andrezaycev@gmail.com>
2017-08-09 23:30:51 +0900

    bottle.ini

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 import re
 import string
 
-app = Bottle()
+app = application = Bottle()
 
 
 @app.error(404)
@@ -28,14 +28,13 @@ def index():
 @app.post('/api')
 def api():
     status = "true"
-    message = "Nothing"
-    #text = request.forms.text.replace("\\n", " ").replace("\\t"," ").replace("  ", " ")
+    message = "None"
     text = request.forms.text
-    for i in [r'\\n', r'\\t',r'•']:
-        text = re.sub(i ," ", text)
-    text = re.sub(r'\s+', ' ' , text)
-    print (text)
-    print (find_url(text))
+    for i in [r'\\n', r'\\t', r'•']:
+        text = re.sub(i, " ", text)
+    text = re.sub(r'\s+', ' ', text)
+    print(text)
+    print(find_url(text))
     try:
         message = summarize(text)
     except Exception as e:
@@ -52,8 +51,10 @@ def error405(error):
     message = "405 Method not allowed"
     return json.dumps(OrderedDict(status=status, message=message))
 
+
 def find_url(text):
-    return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+    return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port="9000", debug=False)
--- posttest.py
+++ posttest.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-with open('testtext2',"r") as fin:
+with open('1.txt',"r") as fin:
     text=fin.read()
-print (text)
+
 r = requests.post("http://10.4.12.35:9000/api", data={'text': text}, headers={"Content-Type": "application/text-plain; charset=UTF-8" })
+print (json.loads(r.text)['message'])
 
 
